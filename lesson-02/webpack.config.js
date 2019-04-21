db43e7322e5c22441b1640bae03bf11ea59a53c5
123428653 <123428653@qq.com>
2019-04-21 14:02:11 +0800

    lesson-02

---
+++ lesson-02/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path]/[name].[ext]',
+        },
+      }
+    ]
+  }
+} 
