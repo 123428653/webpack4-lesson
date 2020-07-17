8353d94fb83308d0666bbbb1e7a51be1e51d0e6b
Qin <123428653@qq.com>
2020-07-17 18:03:31 +0800

    css厂商前缀使用postcss.config.js的方式，删除.browserslistrc文件

--- lesson-03/postcss.config.js
+++ lesson-03/postcss.config.js
@@ -1,9 +1,12 @@
 module.exports = {
 	plugins: {
-		// 编辑目标浏览器：使用package.json中的“browserslist”字段，
-		// 或者使用根目录下的.browserslistrc配置文件
-    autoprefixer: {
-    	// browsers: ['> 1%', 'last 2 version', 'not ie <= 8']
-    }
-  }
+		// 方法一：直接在 webpack.config.js 中的postcss-loader配置
+		// 方法二：或者使用根目录下的.browserslistrc配置文件
+		// 方法三：编辑目标浏览器：使用package.json中的“browserslist”字段，
+		// 方法三：新增postcss.config.js的plugins.autoprefixer.overrideBrowserslist
+		
+	    autoprefixer: {
+	    	 overrideBrowserslist: ['> 1%', 'last 2 version', 'not ie <= 8']
+	    }
+  	}
 }
